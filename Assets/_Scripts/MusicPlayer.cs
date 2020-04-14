08cc40fc9f3acf385129bf0c3503a878b7937f49
Martyn <Martyn_Stewart89@hotmail.com>
2020-04-14 22:44:48 +0100

    S4.93 - World Created + Player Movement

---
+++ Assets/_Scripts/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 1f);
+
+        string[] responses = { "test", "test2", "test3" };
+
+        foreach (string response in responses)
+{
+            string tallResponse = response.ToUpper();
+
+            foreach (char a in tallResponse) {
+                //DoSomethingWith a
+            }
+        }
+    }
+
+    void LoadFirstScene() {
+        Debug.Log("Loading Scene");
+        SceneManager.LoadScene(1);
+
+    }
+
+}
